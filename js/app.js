8e36889a5315ebdda8582a5689e183475924f517
jeffreyshao <jeffreyshao@ufl.edu>
2022-01-02 14:48:42 -0500

    A whole bunch of jQuery stuff

--- js/app.js
+++ js/app.js
@@ -4,14 +4,6 @@ const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
 
-// add fixed class to navbar
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 5) {
-    navbar.classList.add("navbar-fixed");
-  } else {
-    navbar.classList.remove("navbar-fixed");
-  }
-});
 // show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
@@ -21,3 +13,49 @@ closeBtn.addEventListener("click", function () {
 });
 // set year
 date.innerHTML = new Date().getFullYear();
+
+// get all matching HTML from the page, using the class value to try and find it
+var imgs = $(".case-study-img");
+
+var modal = document.getElementById('modal');
+var modalImg = document.getElementById('modal-img');
+var modalOn = false;
+var currentImgIndex;
+
+$(".case-study-img").click(function(){
+  modalImg.src = $(this).attr('src');
+  modal.style.display = "block";
+  modalOn = true;
+  currentImgIndex = jQuery.inArray($(this)[0], imgs);
+  console.log(currentImgIndex);
+});
+
+$("#modal").click(function(){
+  modal.style.display = "none";
+  modalOn = false;
+});
+
+$(window).keydown(function(e) {
+    // e.preventDefault(); //prevent default arrow key behavior
+
+    //left
+    if (modalOn && e.keyCode == 37) {
+      if (currentImgIndex !=0){
+        currentImgIndex--;
+      }
+      else {
+        currentImgIndex = imgs.length-1;
+      }
+    }
+    //right
+    else if (modalOn && e.keyCode == 39) {
+      if (currentImgIndex != imgs.length-1){
+        currentImgIndex++;
+      }
+      else {
+        currentImgIndex = 0;
+      }
+    }
+    modalImg.src = imgs[currentImgIndex].src;
+
+});
