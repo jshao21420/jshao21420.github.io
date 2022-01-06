d3616421c9288f6297b2aad6b36d5f7a01238c83
jeffreyshao <jeffreyshao@ufl.edu>
2022-01-06 17:20:37 -0500

    fixing up some images

--- js/app.js
+++ js/app.js
@@ -17,45 +17,78 @@ date.innerHTML = new Date().getFullYear();
 // get all matching HTML from the page, using the class value to try and find it
 var imgs = $(".case-study-img");
 
+// get the modal element
 var modal = document.getElementById('modal');
+// and the image inside of it
 var modalImg = document.getElementById('modal-img');
-var modalOn = false;
+// for the arrow key function
+var modalOn = 0;
+// for tracking which image is being displayed
 var currentImgIndex;
 
+// when the case study image is clickced
 $(".case-study-img").click(function(){
+  // display the image in the modal
   modalImg.src = $(this).attr('src');
   modal.style.display = "block";
-  modalOn = true;
+  // the modal is on, so the arrow keys should work
+  modalOn = 1;
+  // track which image you are currently displaying
   currentImgIndex = jQuery.inArray($(this)[0], imgs);
-  console.log(currentImgIndex);
 });
 
-$("#modal").click(function(){
+/*
+// turning off the modal
+$(modal).click(function(){
   modal.style.display = "none";
   modalOn = false;
 });
+*/
 
+
+// turning off the modal
+$(modal).click(function(){
+  if(modalOn == 1){
+    modalImg.classList.add("modal-content-extrazoom");
+    modalOn = 2;
+  }
+  else if (modalOn == 2){
+    modalImg.classList.remove("modal-content-extrazoom");
+    modal.style.display = "none";
+    modalOn = 0;
+  }
+});
+
+
+// when a key is pressed
 $(window).keydown(function(e) {
     // e.preventDefault(); //prevent default arrow key behavior
 
-    //left
+    //when the modal is open and the key is the left arrow
     if (modalOn && e.keyCode == 37) {
+      // decrement the index
       if (currentImgIndex !=0){
         currentImgIndex--;
       }
+      // if you are at the start, loop to the end
       else {
         currentImgIndex = imgs.length-1;
       }
     }
-    //right
+
+    //when the modal is open and the key is the right arrow
     else if (modalOn && e.keyCode == 39) {
+      //increment the index
       if (currentImgIndex != imgs.length-1){
         currentImgIndex++;
       }
+      // if you are at the end, loop to the start
       else {
         currentImgIndex = 0;
       }
     }
+
+    // based on previous index setting, set the image
     modalImg.src = imgs[currentImgIndex].src;
 
 });
